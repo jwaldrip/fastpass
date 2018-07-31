6cc7e7b587993884923f916c3fa309b6a9d97b68
jwaldrip <jason@waldrip.net>
2018-07-31 14:13:52 -0600

    calculate time savings

--- src/fastpass/cli/run.cr
+++ src/fastpass/cli/run.cr
@@ -7,6 +7,7 @@ class Fastpass::CLI::RunScript < Admiral::Command
   @uri : URI?
   @spec : Spec?
   @sha : String?
+  @runtime : Float64?
 
   define_help description: "Runs a fast pass script."
   define_flag config, short: c, description: "Location of the config file", default: ".fastpass.yml"
@@ -25,15 +26,12 @@ class Fastpass::CLI::RunScript < Admiral::Command
     print " (took #{(Time.now - start).to_f.round(2)}s)\n"
     puts "🐇  checking status".colorize(:light_yellow)
     response = HTTP::Client.get uri
-    raise "unknown status" unless response.status_code < 400
-    fastpass
+    raise "unknown status" unless response.status_code == 202
+    timesaved = JSON.parse(response.body)["timesaved"].as_f
+    puts "🐇  fastpass (saved #{timesaved.round(2)}s)!".colorize(:light_green)
   rescue e
     @error_io.puts "🐇  error: #{e.message}".colorize(:light_red)
     run_and_report
-  end
-
-  private def fastpass
-    puts "🐇  fastpass!".colorize(:light_green)
   end
 
   private def run_and_report
@@ -47,6 +45,7 @@ class Fastpass::CLI::RunScript < Admiral::Command
       io.rewind
     end
     puts ""
+    start = Time.now
     status = Process.run(
       command: ENV["SHELL"],
       args: ["-eo", "pipefail"],
@@ -54,13 +53,14 @@ class Fastpass::CLI::RunScript < Admiral::Command
       error: @error_io,
       output: @output_io
     )
+    @runtime = (Time.now - start).to_f
     status.success? ? report : Process.exit(status.exit_status)
   end
 
   private def report
     puts ""
     puts "🐇  reporting success".colorize(:light_green)
-    response = HTTP::Client.post uri
+    response = HTTP::Client.post(uri.to_s, form: { "runtime" => @runtime.to_s })
     raise "unable to report" unless response.status_code == 201
   rescue e
     @error_io.puts "🐇  error: #{e.message}".colorize(:light_red)
--- src/fastpass/cli/server.cr
+++ src/fastpass/cli/server.cr
@@ -1,8 +1,11 @@
 require "admiral"
 require "orion"
+require "json"
 
 class Fastpass::CLI::Server < Admiral::Command
-  SHAS = {} of String => Bool
+  SHAS = {} of String => Float64
+
+  class_property saved : Float64 = 0.0
 
   define_help description: "Runs a fast pass server."
 
@@ -11,17 +14,27 @@ class Fastpass::CLI::Server < Admiral::Command
     use HTTP::LogHandler
 
     get "/" do |context|
-      context.response.puts "OK"
+      context.response.puts "Time Saved: #{Server.saved.round(2)}s"
     end
 
     get "/:sha" do |context|
       sha = context.request.path_params["sha"]
-      context.response.status_code = SHAS[sha]? ? 200 : 404
+      if SHAS[sha]?
+        context.response.status_code = 202
+        context.response.content_type = "application/json"
+        JSON.build(context.response) do |builder|
+          Server.saved += SHAS[sha]
+          { timesaved: SHAS[sha] }.to_json(builder)
+        end
+      else
+        context.response.status_code = 404
+      end
     end
 
     post "/:sha" do |context|
+      form_params = HTTP::Params.parse(context.request.body.try(&.gets_to_end).to_s)
       sha = context.request.path_params["sha"]
-      SHAS[sha] = true
+      SHAS[sha] = (form_params["runtime"]? || 0.0).to_f
       context.response.status_code = 201
     end
   end
