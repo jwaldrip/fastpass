30a013ed410c3ae2b6ed65a241a4ef5e320df04d
jwaldrip <jason@waldrip.net>
2018-07-30 21:09:04 -0600

    spec spec yaml

--- src/fastpass/spec/script.cr
+++ src/fastpass/spec/script.cr
@@ -3,8 +3,8 @@ require "yaml"
 class Fastpass::Spec::Script
   YAML.mapping(
     command: String,
-    check_files: Array(String),
-    ignore_files: Array(String),
-    check_environment: Array(String)
+    check_files: { type: Array(String), default: [] of String },
+    check_environment: { type: Array(String), default: [] of String },
+    ignore_files: { type: Array(String), default: [] of String }
   )
 end
